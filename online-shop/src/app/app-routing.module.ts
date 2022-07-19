f784df2412bd2903d73dd48d233a7bdec8bfeef8
Tudor Lechintan <lechintantudor02@gmail.com>
2022-07-19 09:45:05 +0300

    Chapter 3: Implement page not found component

--- online-shop/src/app/app-routing.module.ts
+++ online-shop/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent },
   { path: 'product', component: ProductComponent },
   { path: 'product-list', component: ProductListComponent },
+  { path: '', component: IndexComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- online-shop/src/app/app.module.ts
+++ online-shop/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { IndexComponent } from './index/index.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     IndexComponent,
-    ProductListComponent
+    ProductListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
